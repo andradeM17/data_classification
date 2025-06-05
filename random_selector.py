b5a7b9f6d5b810af3a7ee721a205ba2585e8218e
Mark A <andradem@tcd.ie>
2025-06-05 12:41:45 +0100

    Refromatted repo

--- random_selector.py
+++ random_selector.py
@@ -1,15 +1,17 @@
 import csv
 import random
 
-NUMBER_OF_LINES = 22172
+SOURCE = "NLLB-ENIE.txt"
+NEW_CONTENT = "NLLB-ENIE-samples.csv"
 
-with open("ga.txt") as opus:
+with open(SOURCE) as opus:
     content = opus.readlines()
+    NUMBER_OF_LINES = len(content)
 
-with open('samples.csv', 'w', newline='') as csvfile:
+with open(NEW_CONTENT, 'w', newline='') as csvfile:
     samples = csv.writer(csvfile)
     samples.writerow(["Sample", "Source"])
 
-    for _ in range(100):
+    for _ in range(50):
         line = random.randint(2, NUMBER_OF_LINES - 2)
-        samples.writerow([''.join(content[line-4:line+5]), str("OPUS QED, lines " + str(line-4) + " to " + str(line+5))])
+        samples.writerow([''.join(content[line-4:line+5]), str("OPUS NLLB, lines " + str(line-3) + " to " + str(line+5))])

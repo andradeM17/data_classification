b6712385af49d1a039355790f77ec1ee64c6c286
Mark A <andradem@tcd.ie>
2025-06-05 14:27:49 +0100

    NLLB-ENIE samples

--- random_selector.py
+++ random_selector.py
@@ -1,8 +1,11 @@
 import csv
 import random
 
-SOURCE = "NLLB-ENIE.txt"
-NEW_CONTENT = "NLLB-ENIE-samples.csv"
+CORPUS = "NLLB-ENIE" # <- change this to the desired corpus name
+NUMBER_OF_SAMPLES = 50 # <- adjust this number to change the number of samples
+
+SOURCE = CORPUS + ".txt"
+NEW_CONTENT = CORPUS + "-samples.csv"
 
 with open(SOURCE) as opus:
     content = opus.readlines()
@@ -11,7 +14,6 @@ with open(SOURCE) as opus:
 with open(NEW_CONTENT, 'w', newline='') as csvfile:
     samples = csv.writer(csvfile)
     samples.writerow(["Sample", "Source"])
-
-    for _ in range(50):
+    for _ in range(NUMBER_OF_SAMPLES):
         line = random.randint(2, NUMBER_OF_LINES - 2)
-        samples.writerow([''.join(content[line-4:line+5]), str("OPUS NLLB, lines " + str(line-3) + " to " + str(line+5))])
+        samples.writerow([''.join(content[line-4:line+5]), str("OPUS " + CORPUS + ", lines " + str(line-3) + " to " + str(line+5))])
